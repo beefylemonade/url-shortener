46140905a3f01b28577b87f8c5f27875357beb04
beefylemonade <beefylemonade@gmail.com>
2026-09-12 06:25:40 -0700

    Initial commit of unit test file for Base62Encoder

---
+++ src/test/java/com/beefylemonade/urlshortener/Base62EncoderTests.java
@@ -0,0 +1,48 @@
+package com.beefylemonade.urlshortener;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class Base62EncoderTests {
+
+	@Test
+	void encode_zeroInput_returnsZeroString() {
+		long input = 0;
+		String expectedOutput = "0";
+		assertEquals(expectedOutput, Base62Encoder.encode(input));
+	}
+
+	@Test
+	void encode_alphabetBoundary_returnsZ() {
+		long input = 61;
+		String expectedOutput = "z";
+		assertEquals(expectedOutput, Base62Encoder.encode(input));
+	}
+
+	@Test
+	void encode_baseRollover_returnsTwoDigits() {
+		long input = 62;
+		String expectedOutput = "10";
+		assertEquals(expectedOutput, Base62Encoder.encode(input));
+	}
+
+	@Test
+	void decode_twoDigitRollover_returns62() {
+		String input = "10";
+		long expectedOutput = 62;
+		assertEquals(expectedOutput, Base62Encoder.decode(input));
+	}
+
+	@Test
+	void decodeEncode_roundTrip_returnsOriginalValue() {
+		long original = 123456;
+		assertEquals(original, Base62Encoder.decode(Base62Encoder.encode(original)));
+	}
+
+	@Test
+	void encodeDecode_largeValue_roundTrips() {
+		long original = 999_999_999_999L;
+		assertEquals(original, Base62Encoder.decode(Base62Encoder.encode(original)));
+	}
+}
